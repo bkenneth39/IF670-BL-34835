a686153d539535e678eb0c76f95effd8a8257c3a
bkenneth39 <bryankenneth0309@gmail.com>
2021-11-24 22:28:36 +0700

    Week 12 uploaded

---
+++ 34835_BryanKenneth_Week12/34835_BryanKenneth_Week12(Challenge)/src/components/MemoryItem.tsx
@@ -0,0 +1,58 @@
+import { IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonGrid } from "@ionic/react"
+import React, { useContext } from "react"
+import MemoriesContext from "../data/memories-context"
+import {GoogleMap, InfoWindow, LoadScript, Marker} from '@react-google-maps/api'
+const MemoryType: React.FC <{memory: any}> = props  => {
+    const memoriesCtx = useContext(MemoriesContext)
+    const containerStyle = {
+        width: '100%',
+        height: '140px'
+      }
+    const memories = memoriesCtx.memories.filter(memory => memory.type === props.memory)
+    return(
+
+       <IonGrid>
+                    {memories.length === 0 && (
+                        <IonRow>
+                            <IonCol className="ion-text-center">
+                                <h2>No good memories found</h2>
+                            </IonCol>
+                        </IonRow>
+                    )}
+
+                    {memories.map(memory => (
+                        <IonRow key={memory.id}>
+                            {/* {memory.type}
+                            {memory.title} */}
+                            <IonCol>
+                                <IonCard>
+                                    {/* <img src={memory.imagePath} alt={memory.title} /> */}
+                                    <IonCardHeader>
+                                        <img src={`${memory.fotoUrl}`} alt={memory.title}/>
+                                        
+                                        <GoogleMap
+                                            mapContainerStyle={containerStyle}
+                                            center={{lat:parseFloat(memory.lat), lng:parseFloat(memory.lng)}}
+                                            zoom={18}
+                                           
+                                            
+                                        >
+                                            <Marker position={{lat:parseFloat(memory.lat), lng:parseFloat(memory.lng)}} />
+                                            <></>
+                                            {/* <InfoWindow position={{lat:lat,lng:lng}}>
+                                            <div>
+                                                <h1 style={{color:'black'}}>Kampus paling keren.</h1>
+                                            </div>
+                                            </InfoWindow> */}
+                                        </GoogleMap>
+                                        <IonCardTitle>{memory.title}</IonCardTitle>
+                                    </IonCardHeader>
+                                </IonCard>
+                            </IonCol>
+                        </IonRow>
+                    ))}
+                </IonGrid>
+    )
+}
+
+export default MemoryType
